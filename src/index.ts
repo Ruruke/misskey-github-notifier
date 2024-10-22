47d49024db3b37f160547b8b1e242ec03ae44bab
ruruke <123709459+Ruruke@users.noreply.github.com>
2024-10-22 19:48:18 +0900

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -17,7 +17,8 @@ const post = async (text: string, home = true) => {
 			text,
 			visibility: home ? 'home' : 'public',
 			noExtractMentions: true,
-			noExtractHashtags: true
+			noExtractHashtags: true,
+			localOnly: true
 		}
 	});
 };
@@ -51,39 +52,6 @@ const server = http.createServer(app.callback());
 
 server.listen(config.port);
 
-handler.on('status', event => {
-	const state = event.state;
-	switch (state) {
-		case 'error':
-		case 'failure':
-			const commit = event.commit;
-			const parent = commit.parents[0];
-
-			// Fetch parent status
-			request({
-				url: `${parent.url}/statuses`,
-				proxy: config.proxy,
-				headers: {
-					'User-Agent': 'misskey'
-				}
-			}, (err, res, body) => {
-				if (err) {
-					console.error(err);
-					return;
-				}
-				const parentStatuses = JSON.parse(body);
-				const parentState = parentStatuses[0]?.state;
-				const stillFailed = parentState === 'failure' || parentState === 'error';
-				if (stillFailed) {
-					post(`⚠️ **BUILD STILL FAILED** ⚠️: ?[${commit.commit.message}](${commit.html_url})`);
-				} else {
-					post(`🚨 **BUILD FAILED** 🚨: → ?[${commit.commit.message}](${commit.html_url}) ←`);
-				}
-			});
-			break;
-	}
-});
-
 handler.on('push', event => {
 	const ref = event.ref;
 	switch (ref) {
@@ -99,130 +67,12 @@ handler.on('push', event => {
 	}
 });
 
-handler.on('issues', event => {
-	const issue = event.issue;
-	const action = event.action;
-	let title: string;
-	switch (action) {
-		case 'opened': title = `💥 Issue opened`; break;
-		case 'closed': title = `💮 Issue closed`; break;
-		case 'reopened': title = `🔥 Issue reopened`; break;
-		default: return;
-	}
-	post(`${title}: #${issue.number} "${issue.title}"\n${issue.html_url}`);
-});
-
-handler.on('issue_comment', event => {
-	const issue = event.issue;
-	const comment = event.comment;
-	const action = event.action;
-	let text: string;
-	switch (action) {
-		case 'created': text = `💬 Commented on "${issue.title}": ${comment.user.login} "<plain>${comment.body}</plain>"\n${comment.html_url}`; break;
-		default: return;
-	}
-	post(text);
-});
-
 handler.on('release', event => {
 	const action = event.action;
 	const release = event.release;
 	let text: string;
 	switch (action) {
 		case 'published': text = `🎁 **NEW RELEASE**: [${release.tag_name}](${release.html_url}) is out. Enjoy!`; break;
-		default: return;
-	}
-	post(text);
-});
-
-handler.on('watch', event => {
-	const sender = event.sender;
-	post(`$[spin ⭐️] Starred by ?[**${sender.login}**](${sender.html_url})`, false);
-});
-
-handler.on('fork', event => {
-	const sender = event.sender;
-	const repo = event.forkee;
-	post(`$[spin.y 🍴] ?[Forked](${repo.html_url}) by ?[**${sender.login}**](${sender.html_url})`);
-});
-
-handler.on('pull_request', event => {
-	const pr = event.pull_request;
-	const action = event.action;
-	let text: string;
-	switch (action) {
-		case 'opened': text = `📦 New Pull Request: "${pr.title}"\n${pr.html_url}`; break;
-		case 'reopened': text = `🗿 Pull Request Reopened: "${pr.title}"\n${pr.html_url}`; break;
-		case 'closed':
-			text = pr.merged
-				? `💯 Pull Request Merged!: "${pr.title}"\n${pr.html_url}`
-				: `🚫 Pull Request Closed: "${pr.title}"\n${pr.html_url}`;
-			break;
-		default: return;
-	}
-	post(text);
-});
-
-handler.on('pull_request_review_comment', event => {
-	const pr = event.pull_request;
-	const comment = event.comment;
-	const action = event.action;
-	let text: string;
-	switch (action) {
-		case 'created': text = `💬 Review commented on "${pr.title}": ${comment.user.login} "<plain>${comment.body}</plain>"\n${comment.html_url}`; break;
-		default: return;
-	}
-	post(text);
-});
-
-handler.on('pull_request_review', event => {
-	const pr = event.pull_request;
-	const review = event.review;
-	if (review.body === undefined || review.body === null || review.body.length <= 0) return;
-
-	const action = event.action;
-	let text: string;
-	switch (action) {
-		case 'submitted': text = `👀 Review submitted: "${pr.title}": ${review.user.login} "<plain>${review.body}</plain>"\n${review.html_url}`; break;
-		default: return;
-	}
-	post(text);
-});
-
-handler.on('discussion', event => {
-	const discussion = event.discussion;
-	const action = event.action;
-	let title: string;
-	let url: string;
-	switch (action) {
-		case 'created':
-			title = `💭 Discussion opened`;
-			url = discussion.html_url;
-			break;
-		case 'closed':
-			title = `💮 Discussion closed`;
-			url = discussion.html_url;
-			break;
-		case 'reopened':
-			title = `🔥 Discussion reopened`;
-			url = discussion.html_url;
-			break;
-		case 'answered':
-			title = `✅ Discussion marked answer`;
-			url = discussion.answer_html_url;
-			break;
-		default: return;
-	}
-	post(`${title}: #${discussion.number} "${discussion.title}"\n${url}`);
-});
-
-handler.on('discussion_comment', event => {
-	const discussion = event.discussion;
-	const comment = event.comment;
-	const action = event.action;
-	let text: string;
-	switch (action) {
-		case 'created': text = `💬 Commented on "${discussion.title}": ${comment.user.login} "<plain>${comment.body}</plain>"\n${comment.html_url}`; break;
 		default: return;
 	}
 	post(text);
